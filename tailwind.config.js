f4f94d03a19f990378333046aede419449c156b0
AyakaB <ayaka.bando22@gmail.com>
2022-12-07 15:26:49 -0800

    fixed some bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './src/pages//*.{js,jsx,ts,tsx}", "./src/components//*.{js,jsx,ts,tsx}',
+  ],
   // content: ['./*.html'],
   theme: {
     colors: {
