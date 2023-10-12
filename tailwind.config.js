29735a00fc566c60cc594ff4908d7d83f05886f1
a1ya1k2a9@gmail.com <a1ya1k2a9@gmail.com>
2023-10-13 08:27:15 +0900

    added react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    './src/pages//*.{js,jsx,ts,tsx}", "./src/components//*.{js,jsx,ts,tsx}',
-  ],
-  // content: ['./*.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     colors: {
       offWhite: '#fefffe',
