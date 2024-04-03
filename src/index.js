f130fb61a4b2a376666dfdffcbf2a5084f9363ce
a1ya1k2a9@gmail.com <a1ya1k2a9@gmail.com>
2024-04-04 06:56:31 +0900

    added google tag manager

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-000000'
+}
+
+TagManager.initialize(tagManagerArgs)
+...
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
