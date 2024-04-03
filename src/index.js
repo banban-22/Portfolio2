d1b1685b42d0638f6847ec047d3d83b4c56c0802
a1ya1k2a9@gmail.com <a1ya1k2a9@gmail.com>
2024-04-04 07:03:44 +0900

    fixed unneedded bug

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import TagManager from 'react-gtm-module'
+import TagManager from 'react-gtm-module';
 
 const tagManagerArgs = {
-    gtmId: 'G-2MJ7CN51WR'
-}
+  gtmId: 'G-2MJ7CN51WR',
+};
 
-TagManager.initialize(tagManagerArgs)
-...
-
+TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
